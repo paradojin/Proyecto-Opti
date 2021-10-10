7c1a58a9eb71f745f308fc7b1455d6defaa69e0f
MasterZer0x <42559276+MasterZer0x@users.noreply.github.com>
2021-10-10 18:32:25 -0300

    Mejor descripcion y arreglos varios

--- generador_transporte.py
+++ generador_transporte.py
@@ -1,6 +1,8 @@
 import random
 
+
 def print_matrix(matrix):
+    # Imprimir costos en formato matriz
     
     rows = len(matrix)
     columns = len(matrix[0])
@@ -20,6 +22,8 @@ def print_matrix(matrix):
     
 
 def generate_matrix(i, j, value_limit):
+    # Generar matriz, de tamaño i*j, con valores desde 0 hasta value_limit
+    
     matrix = list()
     for h in range(0,i):
         line = list()
@@ -30,6 +34,7 @@ def generate_matrix(i, j, value_limit):
     return matrix
 
 def generate_random_list(size, a, b):
+    # Generar lista de tamaño "size", con valores en un rango "a" y "b"
 
     return_list = list()
 
@@ -54,15 +59,15 @@ def generate_problem ():
     #   - SATISFACER DEMANDA Y/O RESPETAR OFERTA(BALANCEADO O DESBALANCEADO)
     #   - SRS
     
-    
+    # --------------------------------------------------------------------------
+
+    # Definir tamaño de la problematica
     
     problemSize = random.randint(1,3)
     
     # 1: Pequeño
     # 2: Mediano
     # 3: Grande
-
-    # Casos inicialmente considerados Oferta = Demanda y Oferta > Demanda
 
     if problemSize == 1:
         nplantas = random.randint(10,99)
@@ -74,25 +79,34 @@ def generate_problem ():
         nplantas = random.randint(1000,9999)
         nreceptores = random.randint(1000,9999)
 
-    cost_matrix = generate_matrix(nplantas, nreceptores, 100)
-    unitary_matrix  = generate_matrix(nplantas, nreceptores, 1) # Binarios
+    cost_matrix = generate_matrix(nplantas, nreceptores, 100) # Costo unitario de transporte desde i hasta j.
+
+    isMixed = True if random.randint(0,1) else False
+    if(isMixed):
+        unitary_matrix  = generate_matrix(nplantas, nreceptores, 1) # Restriccion de caminos posibles
+    else:
+        unitary_matrix = None
     
-    oferta = generate_random_list(nplantas, 0, 100)
-    demanda = generate_random_list(nreceptores, 0, 100)
-
-    # ¿Casos de sobredemanda no resolubles?
-
-    while(oferta < demanda):
-        demanda = generate_random_list(nreceptores, 0, 100)
-
-    # isMixed = True if random.randint(0,1) else False Definir
+    oferta = generate_random_list(nplantas, 0, 100) # Lista de productos ofertados por cada planta
+    demanda = generate_random_list(nreceptores, 0, 100) # Lista de productos solicitados por cada demandante
 
 
-    print(sum(oferta))
-    print(sum(demanda))
+    # --------------------------------------------------------------------------
+    # El siguiente codigo limita la generacion a unicamente problemas factibles
+    # , pero como se pide analizar infactibilidad se ignorará.
+    
+    #while(oferta < demanda):
+        #demanda = generate_random_list(nreceptores, 0, 100)
+    # --------------------------------------------------------------------------
 
+    print("Problema de transporte, con "+str(nplantas)+ " plantas y "+str(nreceptores)+ " demandantes generado.")
+    
+    return (nplantas, nreceptores, oferta, demanda, cost_matrix, unitary_matrix)
+    
 
-
+def get_lingo_input():
+    ...
+    
 
 
 generate_problem()
